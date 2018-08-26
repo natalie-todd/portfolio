a6604adbb9016917581f4c79272a9a7374a70773
Natalie <natalie.todd15@gmail.com>
2018-08-26 13:26:00 -0600

    was trying to render the contact like app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
-import './App.css';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Front from './components/Front/Front';
+import Projects from './components/Projects/Projects';
+import Contact from './components/Contact/Contact';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      
+      <Front />
+            <h1>Test test</h1>
+            <Projects />
+            <Contact />
       </div>
     );
   }
