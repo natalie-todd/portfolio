1256e367c373f10c1dbf3167a788d96450071f3e
Natalie <natalie.todd15@gmail.com>
2018-08-26 13:26:22 -0600

    at least getting meaningful errors now

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,23 +1,16 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import './App.css';
 
-import Front from './components/Front/Front';
-import Projects from './components/Projects/Projects';
-import Contact from './components/Contact/Contact';
-
-class App extends Component {
-    // state = { jobs: [] };
-    render() {
-        return (
-            <React.Fragment>
-            <Front />
-            <h1>Test test</h1>
-            <Projects />
-            <Contact />
-            </React.Fragment>
-    )
-    }
-}
-export default App;
+// class App extends Component {
+//     // state = { jobs: [] };
+//     render() {
+//         return (
+//             {/* <React.Fragment>
+            
+//             </React.Fragment> */}
+//     )
+//     }
+// }
+// export default Contact;
