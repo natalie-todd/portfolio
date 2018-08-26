42cb36c7efaf1adf2768c9039e9315d0c2b69acb
Natalie <natalie.todd15@gmail.com>
2018-08-26 13:56:33 -0600

    jumbotron added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import { Alert } from 'reactstrap';
 
 import Front from './components/Front/Front';
 import Projects from './components/Projects/Projects';
@@ -10,10 +12,14 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-      <Front />
-            <h1>App Test test</h1>
-            <Projects />
-            <Contact />
+        <Front />
+        <div>
+          <Alert color="primary">
+            This is a primary alert — check it out!
+      </Alert>
+        </div>
+        <Projects />
+        <Contact />
       </div>
     );
   }
--- src/components/Front/Front.js
+++ src/components/Front/Front.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
+import { Jumbotron, Button } from 'reactstrap';
 
 const Front = (props) => {
     return (
-                <header>
-                    <p>Front test test</p>
-                </header>
+        <div>
+            <Jumbotron>
+                <h1 className="display-3">Full Stack Developer</h1>
+                <p className="lead">This is a simple hero unit, a simple Jumbotron-style component for calling extra attention to featured content or information.</p>
+                <hr className="my-2" />
+                <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+                <p className="lead">
+                    <Button color="primary">Learn More</Button>
+                </p>
+            </Jumbotron>
+        </div>
     )
 }
 
