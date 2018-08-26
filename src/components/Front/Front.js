98c6dfbc6a68b1d2307a28d945413e2d5e6728c1
Natalie <natalie.todd15@gmail.com>
2018-08-26 15:01:40 -0600

    pug is on the page

--- src/components/Front/Front.js
+++ src/components/Front/Front.js
@@ -1,17 +1,30 @@
 import React, { Component } from 'react';
-import { Jumbotron, Button } from 'reactstrap';
+import PropTypes from "prop-types";
+import { Jumbotron, Container, Button } from 'reactstrap';
+
 
 const Front = (props) => {
+    //not sure if I need any of the prop.Types stuff...
+    Jumbotron.propTypes = {
+        // Pass in a Component to override default element
+        tag: PropTypes.oneOfType([PropTypes.func, PropTypes.string]),
+        fluid: PropTypes.bool,
+        className: PropTypes.string
+      };
+    
     return (
-        <div>
-            <Jumbotron>
+        <div class='pug'>
+            <Jumbotron fluid>
+                <Container fluid>
                 <h1 className="display-3">Full Stack Developer</h1>
-                <p className="lead">This is a simple hero unit, a simple Jumbotron-style component for calling extra attention to featured content or information.</p>
-                <hr className="my-2" />
-                <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+                <p className="lead">Based out of Denver, CO. My skillset
+                    includes React, Vue.js, Node, Express.js, Knex, and
+                    PostgreSQL. If you are an employer looking to hire,
+                    you can get in touch with me here.</p>
                 <p className="lead">
-                    <Button color="primary">Learn More</Button>
+                    <Button color="primary">Projects</Button>
                 </p>
+                </Container>
             </Jumbotron>
         </div>
     )
